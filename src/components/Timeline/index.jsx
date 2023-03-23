5aaed303bd37a5a35fa77081a6c7e299177dbd34
Zeiion <Zeiion@qq.com>
2023-03-23 21:14:17 +0800

    feat: timeline aos

--- src/components/Timeline/index.jsx
+++ src/components/Timeline/index.jsx
@@ -42,33 +42,40 @@ const Timeline = ({
 }) => {
   return (
     <section id="cd-timeline" class="cd-container">
-      {list.map(({ title, type, date, dateRange, description, tags }) => {
-        return (
-          <div class="cd-timeline-block">
-            <div class="cd-timeline-img cd-picture"></div>
-            <div class="cd-timeline-content">
-              <h2>{title}</h2>
-              <div class="timeline-content-info">
-                <span class="timeline-content-info-title">
-                  <i class="fa fa-certificate" aria-hidden="true"></i>
-                  {type}
-                </span>
-                <span class="timeline-content-info-date">
-                  <i class="fa fa-calendar-o" aria-hidden="true"></i>
-                  {dateRange}
-                </span>
+      {list.map(
+        ({ title, type, date, dateRange, description, tags }, index) => {
+          return (
+            <div
+              class="cd-timeline-block"
+              data-aos="fade-up"
+              data-aos-delay={100 * index}
+              key={index}
+            >
+              <div class="cd-timeline-img cd-picture"></div>
+              <div class="cd-timeline-content">
+                <h2>{title}</h2>
+                <div class="timeline-content-info">
+                  <span class="timeline-content-info-title">
+                    <i class="fa fa-certificate" aria-hidden="true"></i>
+                    {type}
+                  </span>
+                  <span class="timeline-content-info-date">
+                    <i class="fa fa-calendar-o" aria-hidden="true"></i>
+                    {dateRange}
+                  </span>
+                </div>
+                <p>{description}</p>
+                <span class="cd-date">{date}</span>
+                <ul class="content-skills">
+                  {tags.map((tag, index) => (
+                    <li key={index}>{tag}</li>
+                  ))}
+                </ul>
               </div>
-              <p>{description}</p>
-              <span class="cd-date">{date}</span>
-              <ul class="content-skills">
-                {tags.map((tag, index) => (
-                  <li key={index}>{tag}</li>
-                ))}
-              </ul>
             </div>
-          </div>
-        );
-      })}
+          );
+        },
+      )}
     </section>
   );
 };

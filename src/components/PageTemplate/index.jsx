7a291b6affdb140b5b1dce72b4a9641b9099633d
Zeiion <Zeiion@qq.com>
2023-03-28 22:05:53 +0800

    feat: add card style

--- src/components/PageTemplate/index.jsx
+++ src/components/PageTemplate/index.jsx
@@ -24,7 +24,10 @@ const PageTemplate = ({ title, subTitle, children }) => {
               {/* Page header */}
               <div className="pb-4 mx-auto text-center max-w-7xl md:pb-8 md:text-left">
                 <h1 className="h1" data-aos="fade-up">
-                  {title} <br />{' '}
+                  <span className="relative">
+                    <div className="filter-bg"></div>
+                    {title} <br />{' '}
+                  </span>
                   <span className="mt-2 h2 font-architects-daughter">
                     {subTitle}
                   </span>
